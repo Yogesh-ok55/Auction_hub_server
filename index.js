0a8fdecaf5a66bb3d9bb53859c04094466a62621
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2025-02-25 00:52:36 +0530

    connection with databases(mysql)

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const { configDotenv } = require("dotenv");
 const express = require("express");
 const app = express();
-configDotenv()
-
+const mysql = require("mysql2")
 const authRouter = require("./router/auth")
+configDotenv();
+
+const db = mysql.createConnection({
+    host: process.env.host,    
+    user: process.env.user,         
+    password: process.env.password,
+    database: process.env.database ,
+    port: process.env.database_port
+})
+
+db.connect((err)=>{
+    if(err){
+        console.log(err);
+        return ;
+    }
+    else{
+        console.log("connected successfully")
+    }
+})
+
+
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("app running");
-})
+app.get("/", (req, res) => {
+    res.send("<h1>Server is Running</h1>")
+});
 
 app.use("/signup",authRouter);
 
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log(`server running on port ${process.env.PORT}`)
+    console.log(`server is running`)
 })
