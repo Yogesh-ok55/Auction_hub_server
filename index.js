d85ee6b66f1c50cd620ea425c9c61f35a5464929
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2025-02-25 09:59:40 +0530

    database create, all required tables created and connected

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const { configDotenv } = require("dotenv");
 const express = require("express");
 const app = express();
-const mysql = require("mysql2")
+const createConnection = require("./database/connections")
+
 const authRouter = require("./router/auth")
 configDotenv();
 
-const db = mysql.createConnection({
-    host: process.env.host,    
-    user: process.env.user,         
-    password: process.env.password,
-    database: process.env.database ,
-    port: process.env.database_port
-})
-
-db.connect((err)=>{
-    if(err){
-        console.log(err);
-        return ;
-    }
-    else{
-        console.log("connected successfully")
-    }
-})
-
+createConnection();
 
 
 app.use(express.json());
